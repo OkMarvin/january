91fc37eb4e9538cc12064a11adc6a8bb2039871e
chenxsan <chenxsan@gmail.com>
2019-07-09 18:52:29 +0800

    Use functional component

--- src/components/Html.js
+++ src/components/Html.js
@@ -2,40 +2,44 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Header from './Header'
 import Footer from './Footer'
+
+// Don't do this: import 'sanitize.css'
+// it won't work
 import '../../node_modules/sanitize.css'
 import '../../node_modules/prismjs/themes/prism-okaidia.css'
+
 import '../common.css'
-export default class Html extends React.Component {
-  static propTypes = {
-    children: PropTypes.node,
+
+export default function Html(props) {
+  const { site, permalink } = props
+  const { title: siteTitle, logo, menu } = site
+  return (
+    <>
+      <Header
+        siteTitle={siteTitle}
+        menu={menu}
+        currentUrl={permalink}
+        logo={logo}
+      />
+      {props.children}
+      <Footer {...site} />
+    </>
+  )
+}
+
+Html.propTypes = {
+  children: PropTypes.node,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  permalink: PropTypes.string,
+  site: PropTypes.shape({
+    lang: PropTypes.string.isRequired,
+    url: PropTypes.string,
     title: PropTypes.string.isRequired,
-    description: PropTypes.string,
-    permalink: PropTypes.string,
-    site: PropTypes.shape({
-      lang: PropTypes.string.isRequired,
-      url: PropTypes.string,
-      title: PropTypes.string.isRequired,
-      logo: PropTypes.string,
-      menu: PropTypes.array
-    })
-  }
-  static defaultProps = {
-    description: ''
-  }
-  render() {
-    const { site, permalink } = this.props
-    const { title: siteTitle, logo, menu } = site
-    return (
-      <>
-        <Header
-          siteTitle={siteTitle}
-          menu={menu}
-          currentUrl={permalink}
-          logo={logo}
-        />
-        {this.props.children}
-        <Footer {...site} />
-      </>
-    )
-  }
+    logo: PropTypes.string,
+    menu: PropTypes.array
+  })
+}
+Html.defaultProps = {
+  description: ''
 }
