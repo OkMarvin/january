afc90440b0991ded97b6542e73bdd11a15dac50d
chenxsan <chenxsan@gmail.com>
2019-07-09 19:24:02 +0800

    Use functional component

--- src/templates/index.js
+++ src/templates/index.js
@@ -8,66 +8,64 @@ import { getYear } from 'date-fns'
 import { bps, lineHeight, mobileScale, scale } from '../constants'
 import ArchiveItem from '../components/ArchiveItem'
 import Pagination from '../components/Pagination'
-class Index extends React.Component {
-  render() {
-    const { posts, site } = this.props
-    const { paginate } = site
-    const archives = groupBy(posts, file =>
-      getYear(new Date(file.datePublished))
+function Index(props) {
+  const { posts, site } = props
+  const { paginate } = site
+  const archives = groupBy(posts, file => getYear(new Date(file.datePublished)))
+  Object.keys(archives).map(year => {
+    archives[year] = archives[year].sort(
+      (a, b) => new Date(b.datePublished, new Date(a.datePublished))
     )
-    Object.keys(archives).map(year => {
-      archives[year] = archives[year].sort(
-        (a, b) => new Date(b.datePublished, new Date(a.datePublished))
-      )
-    })
-    return (
-      <>
-        <Main>
-          {Object.keys(archives)
-            .sort((a, b) => b - a)
-            .map(year => {
-              return (
-                <React.Fragment key={year}>
-                  <Block
-                    component="h2"
-                    fontFamily="Courier, monospace"
-                    fontSize={`${Math.pow(mobileScale, 3)}rem`}
-                    marginTop={`${lineHeight * 2}rem`}
-                    marginBottom={`${lineHeight / 2}rem`}
-                    fontWeight="bold"
-                    lineHeight={lineHeight - 0.3}
-                    mediaQueries={bps}
-                    lgFontSize={`${Math.pow(scale, 4)}rem`}
-                    lgMarginTop={`${lineHeight * 2}rem`}
-                    lgMarginBottom={0}
-                  >
-                    {year}
-                  </Block>
-                  {archives[year].map(d => (
-                    <ArchiveItem key={d.permalink} {...d} />
-                  ))}
-                </React.Fragment>
-              )
-            })}
-          {paginate && this.props.paginator && (
-            <Pagination {...this.props.paginator} paginate={paginate} />
-          )}
-        </Main>
-      </>
-    )
-  }
+  })
+  return (
+    <Main>
+      {Object.keys(archives)
+        .sort((a, b) => b - a)
+        .map(year => {
+          return (
+            <React.Fragment key={year}>
+              <Block
+                component="h2"
+                fontFamily="Courier, monospace"
+                fontSize={`${Math.pow(mobileScale, 3)}rem`}
+                marginTop={`${lineHeight * 2}rem`}
+                marginBottom={`${lineHeight / 2}rem`}
+                fontWeight="bold"
+                lineHeight={lineHeight - 0.3}
+                mediaQueries={bps}
+                lgFontSize={`${Math.pow(scale, 4)}rem`}
+                lgMarginTop={`${lineHeight * 2}rem`}
+                lgMarginBottom={0}
+              >
+                {year}
+              </Block>
+              {archives[year].map(d => (
+                <ArchiveItem key={d.permalink} {...d} />
+              ))}
+            </React.Fragment>
+          )
+        })}
+      {paginate && props.paginator && (
+        <Pagination {...props.paginator} paginate={paginate} />
+      )}
+    </Main>
+  )
 }
 Index.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   site: PropTypes.shape({
-    menu: PropTypes.array
+    menu: PropTypes.array,
+    paginate: PropTypes.number
   }),
   data: PropTypes.array,
-  paginator: PropTypes.object
+  paginator: PropTypes.object,
+  posts: PropTypes.array
 }
-export default props => (
-  <Html {...props}>
-    <Index {...props} />
-  </Html>
-)
+export default function IndexTemplate(props) {
+  return (
+    <Html {...props}>
+      <Index {...props} />
+    </Html>
+  )
+}
