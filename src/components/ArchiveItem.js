1776ee3f257b674dbfa07d4f886d2618ca58e366
chenxsan <chenxsan@gmail.com>
2019-08-20 18:53:30 +0800

    Update fontSize

--- src/components/ArchiveItem.js
+++ src/components/ArchiveItem.js
@@ -6,18 +6,16 @@ import { Col, Inline, InlineBlock } from 'jsxstyle'
 import { format } from 'date-fns'
 const ArchiveItem = ({ title, datePublished, permalink }) => (
   <Col padding="1rem 0" mediaQueries={bps} borderBottom="1px solid #ddd">
-    <InlineBlock color="rgb(118, 118, 118)">
+    <InlineBlock color="rgb(118, 118, 118)" lineHeight={1}>
       {format(datePublished, 'YYYY/MM/DD')}
     </InlineBlock>
     <Inline
       props={{
         to: permalink
       }}
-      mediaQueries={bps}
       component={Link}
       textDecoration="none"
       fontSize="1rem"
-      smFontSize="1.5rem"
       color="#333"
       linkColor="#333"
       hoverTextDecoration="none"
